32382d3b84362481761ece5b1882e69d47d976d3
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-12 13:49:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+const findMatching = (drivers, name) => {
+  return drivers.filter(
+    (driver) => (driver = name && driver.toUpperCase() === name.toUpperCase())
+  );
+};
+
+const fuzzyMatch = (drivers, name) => {
+  return drivers.filter((driver) => driver.startsWith(name));
+};
+
+
+const driversObj = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+const matchName = (driversObj, name) => {
+  return driversObj.filter((driver) => driver.name === name);
+};
+
+console.log(matchName (driversObj, "Bobby"));
